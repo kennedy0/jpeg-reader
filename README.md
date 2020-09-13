7dafaa36f99899a6491efe3ac3a2b0bb16f0a537
andrewjacobkennedy <andrewjacobkennedy@gmail.com>
2020-09-12 18:02:40 -0700

    Simplified using the correct specs

---
+++ README.md
@@ -0,0 +1,13 @@
+# jpeg reader
+
+Basic, pure python jpeg reader
+
+
+Handy links:
+============
+
+https://en.wikipedia.org/wiki/JPEG#Syntax_and_structure
+
+https://www.w3.org/Graphics/JPEG/itu-t81.pdf
+
+http://vip.sugovica.hu/Sardi/kepnezo/JPEG%20File%20Layout%20and%20Format.htm
--- main.py
+++ main.py
@@ -2,32 +2,53 @@ import os
 import struct
 
 
-def read_jpeg(fp):
-    print(f"reading {os.path.abspath(fp)}")
-    with open(fp, 'rb') as f:
-        soi = struct.unpack('>H', f.read(2))[0]  # Start of image
-        assert soi == 0xffd8, "File is not a JPEG file!"
-        print(f"soi: {format(soi, 'x')}")
+class JpegFile:
+    SOI = 0xd8  # Start of Image
+    SOF0 = 0xc0  # Start of Frame (baseline DCT)
+    SOF2 = 0xc2  # Start of Frame (progressive DCT)
+    DHT = 0xc4  # Define Huffman Table(s)
+    DQT = 0xdb  # Define Quantization Table(s)
+    DRI = 0xdd  # Define Restart Interval
+    SOS = 0xda  # Start of Scan
+    COM = 0xfe  # Comment
+    EOI = 0xd9  # End of Image
 
-        app_marker = struct.unpack('>H', f.read(2))[0]
-        if app_marker not in [0xffe0, 0xffe1]:
-            raise IOError(f"Unknown app marker: {format(app_marker, 'x')}")
-        print(f"app marker: {format(app_marker, 'x')}")
+    def __init__(self, file_path):
+        self._file_path = file_path
+        self._resolution = (None, None)
+        self._par = None
 
-        marker_length = struct.unpack('>H', f.read(2))[0]
-        print(f"app marker length: {marker_length}")
+        self._read_file()
 
-        identifier = struct.unpack('>4s', f.read(4))[0]
-        identifier = identifier.decode('utf-8')
-        f.seek(1, os.SEEK_CUR)
+    @property
+    def resolution(self):
+        return self._resolution
 
-        version_major, version_minor = struct.unpack('>2B', f.read(2))
+    def _read_file(self):
+        with open(self._file_path, 'rb') as f:
+            soi = struct.unpack('>H', f.read(2))[0]  # Start of image
+            assert soi == 0xffd8, f"File is not a JPEG file: {self._file_path}"
 
-        print(f"format version: {identifier} {version_major}.{version_minor}")
+            b = f.read(1)
+            while b and ord(b) != self.EOI:
+                while ord(b) != 0xff:
+                    # FF indicates start of a marker
+                    b = f.read(1)
+                while ord(b) == 0xff:
+                    # Skip any consecutive FF bytes (used as fill bytes for padding)
+                    b = f.read(1)
 
+                if ord(b) == self.SOF0:
+                    f.seek(3, os.SEEK_CUR)
+                    width, height = struct.unpack('>2H', f.read(4))
+                    self._resolution = (width, height)
 
+                b = f.read(1)
 
 
 if __name__ == "__main__":
-    read_jpeg("img_paint.jpg")  # JFIF
-    # read_jpeg("img_photoshop.jpg")  # EXIF
+    jfif = JpegFile("img_paint.jpg")
+    print(jfif.resolution)
+
+    exif = JpegFile("img_photoshop.jpg")
+    print(exif.resolution)
